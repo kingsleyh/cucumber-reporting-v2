16e8677dedbbb8de7d1ba83fac5cde1c54565053
Kingsley Hendrickse <kings@electron.home>
2013-12-04 22:35:58 +0000

    the long road starts again.....

---
+++ src/main/java/net/masterthought/cucumber/ReportParser.java
@@ -0,0 +1,12 @@
+package net.masterthought.cucumber;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ReportParser {
+
+    public ReportParser(List<String> jsonReportFiles) throws IOException {
+
+    }
+
+}
